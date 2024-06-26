e855e4587c6df225b5ca8f3d2bbdecb523ed325a
Dalia Ahmed <da257@njit.edu>
2024-06-26 13:54:18 -0400

    components add

---
+++ src/components/AddTransaction.jsx
@@ -0,0 +1,29 @@
+import React, {useState} from 'react'
+
+const AddTransaction = () => {
+
+    const [text, setText] = useState('')
+    const [amount, setAmount] = useState(0)
+
+  return (
+    <>
+        <h3>Add new Transaction</h3>
+        <form>
+            <div className="form-control">
+                <label htmlFor="text">Text</label>
+                <input id='text' type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder='Enter Text...' />
+            </div>
+            <div className="form-control">
+                <label htmlFor="amount">
+                    Amount <br />
+                    (negative - expense, positive - income)
+                </label>
+                <input id='amount' type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder='Enter amount...' />
+            </div>
+            <button className='btn'>Add Transaction</button>
+        </form>
+    </>
+  )
+}
+
+export default AddTransaction
